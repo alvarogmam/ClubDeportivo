806337c8c28408f765425f5c5abbb073ed241c60
Alvaro Mamani <alvarogabriel@outlook.com.ar>
2025-11-14 23:57:21 -0300

    Se crearon las clases modeo socios y cuota para poder relacionar los socios de nuestra base de datos con los que creamos en nuestra app

---
+++ Modelos/Cuota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivo.Modelos
+{
+    public class Cuota
+    {
+        public int IdCuota { get; set; }
+        public int IdSocio { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string Estado { get; set; }
+    }
+}
---
+++ Modelos/Socio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivo.Modelos
+{
+    public class Socio
+    {
+        public int IdSocio { get; set; }
+        public int IdPersona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni { get; set; }
+    }
+}
